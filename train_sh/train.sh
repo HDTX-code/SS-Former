00d502c51f2f395c3747498c6c377bc9a8f96233
HDTX-code <1252900424@qq.com>
2022-06-29 21:46:21 +0800

    同步

--- train_sh/train.sh
+++ train_sh/train.sh
@@ -1,4 +1,4 @@
 conda activate homefun
 nohup python /Home/atr2/homefun/zhf/SS-Former/train.py --GPU 0 >weights/log_L_All.txt 2>&1 &
 # source /Home/atr2/homefun/zhf/SS-Former/train_sh/train.sh
-# CUDA_VISIBLE_DEVICES=3,6 nohup python -m torch.distributed.launch --nproc_per_node=2 main.py >weights/log_L_All.txt 2>&1 &
+# CUDA_VISIBLE_DEVICES=3,6 nohup python -m torch.distributed.launch --nproc_per_node=2 train_mutil_GPU.py >weights/log_L_All.txt 2>&1 &
