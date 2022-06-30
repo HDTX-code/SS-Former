dcdcc52b8bc65d7902412d2a99fe75428b150406
HDTX-code <1252900424@qq.com>
2022-06-30 19:46:26 +0800

    新增提交模块

--- submit.py
+++ submit.py
@@ -156,9 +156,6 @@ def main(args):
         del df_ssub['predicted']
         sub_df = df_ssub.merge(sub_df, on=['id', 'class'])
         assert len(sub_df) == len(df_ssub)
-    else:
-        del class_df['path']
-        sub_df = class_df.merge(sub_df, on=['id', 'class'])
     sub_df[['id', 'class', 'predicted']].to_csv(os.path.join(args.save_dir, 'submission.csv'), index=False)
 
 
